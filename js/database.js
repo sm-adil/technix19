8ccce5aa07b66d7890a0dab568226af0db3f7246
Sheik Mohammed Adil <adilsheik98@gmail.com>
2019-04-03 22:37:46 +0530

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,76 @@
+module.exports = {
+    events: [
+        {
+            "id": 1,
+            "title": "Pick Talk",
+            "date": "25-04-2019",
+            "venue": "TBD",
+            "quote": "lorem",
+            "body": "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus consectetur officia itaque modi laboriosam quibusdam nisi quis delectus cupiditate assumenda sequi atque porro, possimus, blanditiis placeat tenetur. Alias, amet blanditiis.</p>",
+            "imageURl": "./images/events/pick_talk.jpg"
+        },
+        {
+            "id": 2,
+            "title": "Pick Talk",
+            "date": "25-04-2019",
+            "venue": "TBD",
+            "quote": "lorem",
+            "body": "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus consectetur officia itaque modi laboriosam quibusdam nisi quis delectus cupiditate assumenda sequi atque porro, possimus, blanditiis placeat tenetur. Alias, amet blanditiis.</p>",
+            "imageURl": "./images/events/pick_talk.jpg"
+        },
+        {
+            "id": 3,
+            "title": "Pick Talk",
+            "date": "25-04-2019",
+            "venue": "TBD",
+            "quote": "lorem",
+            "body": "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus consectetur officia itaque modi laboriosam quibusdam nisi quis delectus cupiditate assumenda sequi atque porro, possimus, blanditiis placeat tenetur. Alias, amet blanditiis.</p>",
+            "imageURl": "./images/events/pick_talk.jpg"
+        },
+        {
+            "id": 4,
+            "title": "Pick Talk",
+            "date": "25-04-2019",
+            "venue": "TBD",
+            "quote": "lorem",
+            "body": "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus consectetur officia itaque modi laboriosam quibusdam nisi quis delectus cupiditate assumenda sequi atque porro, possimus, blanditiis placeat tenetur. Alias, amet blanditiis.</p>",
+            "imageURl": "./images/events/pick_talk.jpg"
+        },
+        {
+            "id": 5,
+            "title": "Pick Talk",
+            "date": "25-04-2019",
+            "venue": "TBD",
+            "quote": "lorem",
+            "body": "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus consectetur officia itaque modi laboriosam quibusdam nisi quis delectus cupiditate assumenda sequi atque porro, possimus, blanditiis placeat tenetur. Alias, amet blanditiis.</p>",
+            "imageURl": "./images/events/pick_talk.jpg"
+        },
+        {
+            "id": 6,
+            "title": "Pick Talk",
+            "date": "25-04-2019",
+            "venue": "TBD",
+            "quote": "lorem",
+            "body": "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus consectetur officia itaque modi laboriosam quibusdam nisi quis delectus cupiditate assumenda sequi atque porro, possimus, blanditiis placeat tenetur. Alias, amet blanditiis.</p>",
+            "imageURl": "./images/events/pick_talk.jpg"
+        },
+        {
+            "id": 7,
+            "title": "Pick Talk",
+            "date": "25-04-2019",
+            "venue": "TBD",
+            "quote": "lorem",
+            "body": "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus consectetur officia itaque modi laboriosam quibusdam nisi quis delectus cupiditate assumenda sequi atque porro, possimus, blanditiis placeat tenetur. Alias, amet blanditiis.</p>",
+            "imageURl": "./images/events/pick_talk.jpg"
+        },
+        {
+            "id": 8,
+            "title": "Pick Talk",
+            "date": "25-04-2019",
+            "venue": "TBD",
+            "quote": "lorem",
+            "body": "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus consectetur officia itaque modi laboriosam quibusdam nisi quis delectus cupiditate assumenda sequi atque porro, possimus, blanditiis placeat tenetur. Alias, amet blanditiis.</p>",
+            "imageURl": "./images/events/pick_talk.jpg"
+        },  
+    ]
+}
